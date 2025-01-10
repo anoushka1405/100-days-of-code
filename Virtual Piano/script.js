042bbe694d4a7c50368a967e3d578cba8330efd7
Anoushka Gupta <anoushka142006@gmail.com>
2025-01-10 21:27:45 +0530

    Add files via upload
    day 8,9,10

---
+++ Virtual Piano/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const keys = document.querySelectorAll('.key');
+
+    keys.forEach(key => {
+        key.addEventListener('click', playSound);
+    });
+
+    function playSound(event) {
+        const key = event.target;
+        const keyData = key.getAttribute('data-key');
+        console.log(`Key pressed: ${keyData}`); 
+        if (keyData) {
+            const audio = new Audio(`sounds/${keyData}.mp3`);
+            audio.play();
+        }
+    }
+    
+});
